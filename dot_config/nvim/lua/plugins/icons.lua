1a98eb2a009aafe47902b2f213ad7b226a86c330
e99train <e99train@gmail.com>
2025-02-26 16:16:02 -0700

    Custom file icons and updates

---
+++ dot_config/nvim/lua/plugins/icons.lua
@@ -0,0 +1,33 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    file = {
+      ["Fastfile"] = {
+        glyph = "",
+        hl = "MiniIconsRed",
+      },
+      ["Appfile"] = {
+        glyph = "",
+        hl = "MiniIconsRed",
+      },
+      ["Deliverfile"] = {
+        glyph = "",
+        hl = "MiniIconsRed",
+      },
+      ["Matchfile"] = {
+        glyph = "",
+        hl = "MiniIconsRed",
+      },
+      ["Pluginfile"] = {
+        glyph = "",
+        hl = "MiniIconsRed",
+      },
+    },
+    extension = {
+      ["lock"] = {
+        glyph = "",
+        hl = "MiniIconsGrey",
+      },
+    },
+  },
+}
